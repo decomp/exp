f4a63552b0654da67f9040d427ea774ed17ada75
mewmew <rnd0x00@gmail.com>
2015-06-30 19:57:14 +0200

    cmd/bin2c: Add support for XOR instructions.

--- cmd/bin2c/convert.go
+++ cmd/bin2c/convert.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"go/ast"
+	"go/printer"
+	"go/token"
+	"os"
 
 	"github.com/mewkiz/pkg/errutil"
 	"rsc.io/x86/x86asm"
@@ -16,15 +20,101 @@ func convert(text []byte) error {
 // code.
 func convertFunc(text []byte, offset int) error {
 	for {
+		// Decode instruction.
 		inst, err := x86asm.Decode(text[offset:], 32)
 		if err != nil {
 			return errutil.Err(err)
 		}
-		fmt.Println(inst)
+		fmt.Println("inst:", inst)
+
+		// Parse instruction.
+		node, err := parseInst(inst)
+		if err != nil {
+			return errutil.Err(err)
+		}
+		if node != nil {
+			fmt.Println("node:", node)
+			//ast.Print(token.NewFileSet(), node)
+			printer.Fprint(os.Stdout, token.NewFileSet(), node)
+			fmt.Println()
+		}
+
+		// Next.
 		offset += inst.Len
 		if inst.Op == x86asm.RET {
 			break
 		}
 	}
 	return nil
+}
+
+// parseInst parses the given assembly instruction and returns a corresponding
+// Go statement.
+func parseInst(inst x86asm.Inst) (ast.Stmt, error) {
+	switch inst.Op {
+	case x86asm.RET:
+		return &ast.ReturnStmt{}, nil
+	case x86asm.XOR:
+		return parseBinaryInst(inst)
+	case x86asm.PUSH, x86asm.POP:
+		// ignore for now.
+	default:
+		return nil, errutil.Newf("support for opcode %v not yet implemented", inst.Op)
+	}
+	return nil, nil
+}
+
+// regNames maps from register names to their corresponding Go identifiers.
+var regNames = map[string]*ast.Ident{
+	"EAX": ast.NewIdent("eax"),
+}
+
+// parseArg parses the given assembly instruction argument and returns a
+// corresponding Go expression.
+func parseArg(arg x86asm.Arg) (ast.Expr, error) {
+	switch arg := arg.(type) {
+	case x86asm.Reg:
+		reg, ok := regNames[arg.String()]
+		if !ok {
+			return nil, errutil.Newf("unable to lookup identifer for register %q", arg)
+		}
+		return reg, nil
+		return nil, errutil.Newf("support for type %T not yet implemented", arg)
+	case x86asm.Mem:
+		return nil, errutil.Newf("support for type %T not yet implemented", arg)
+	case x86asm.Imm:
+		return nil, errutil.Newf("support for type %T not yet implemented", arg)
+	case x86asm.Rel:
+		return nil, errutil.Newf("support for type %T not yet implemented", arg)
+	default:
+		return nil, errutil.Newf("support for type %T not yet implemented", arg)
+	}
+}
+
+// parseBinaryInst parses the given binary instruction and returns a
+// corresponding Go statement.
+func parseBinaryInst(inst x86asm.Inst) (ast.Stmt, error) {
+	x, err := parseArg(inst.Args[0])
+	if err != nil {
+		return nil, errutil.Err(err)
+	}
+	y, err := parseArg(inst.Args[1])
+	if err != nil {
+		return nil, errutil.Err(err)
+	}
+	var op token.Token
+	switch inst.Op {
+	case x86asm.XOR:
+		op = token.XOR
+	default:
+		return nil, errutil.Newf("support for opcode %v not yet implemented", inst.Op)
+	}
+	rhs := &ast.BinaryExpr{X: x, Op: op, Y: y}
+	lhs := x
+	assign := &ast.AssignStmt{
+		Lhs: []ast.Expr{lhs},
+		Tok: token.ASSIGN,
+		Rhs: []ast.Expr{rhs},
+	}
+	return assign, nil
 }

62f2e0511d2edcce86a6fd155ec3d05bc3516918
mewmew <rnd0x00@gmail.com>
2017-06-01 23:06:41 +0200

    disasm/x86: add Arg, Reg and Mem arguments of instructions

--- disasm/x86/arg.go
+++ disasm/x86/arg.go
@@ -8,6 +8,104 @@ import (
 	"golang.org/x/arch/x86/x86asm"
 )
 
+// === [ arguments ] ===========================================================
+
+// Arg returns the i:th argument of the instruction.
+func (inst *Inst) Arg(i int) *Arg {
+	return NewArg(inst.Args[i], inst)
+}
+
+// Reg returns the register at the i:th argument of the instruction.
+func (inst *Inst) Reg(i int) *Reg {
+	return NewReg(inst.Args[i], inst)
+}
+
+// Mem returns the memory reference at the i:th argument of the instruction.
+func (inst *Inst) Mem(i int) *Mem {
+	return NewMem(inst.Args[i], inst)
+}
+
+// --- [ argument ] ------------------------------------------------------------
+
+// An Arg is a single x86 instruction argument.
+type Arg struct {
+	// x86 instruction argument.
+	x86asm.Arg
+	// Parent instruction; used to calculate relative offsets and retrieve
+	// symbolic execution information.
+	parent *Inst
+}
+
+// NewArg returns a new x86 argument with the given parent instruction.
+func NewArg(arg x86asm.Arg, parent *Inst) *Arg {
+	return &Arg{
+		Arg:    arg,
+		parent: parent,
+	}
+}
+
+// --- [ register ] ------------------------------------------------------------
+
+// A Reg is a single x86 register.
+type Reg struct {
+	// x86 register.
+	x86asm.Reg
+	// Parent instruction; used to retrieve symbolic execution information.
+	parent *Inst
+}
+
+// NewReg returns a new x86 register argument with the given parent instruction.
+func NewReg(arg x86asm.Arg, parent *Inst) *Reg {
+	reg, ok := arg.(x86asm.Reg)
+	if !ok {
+		panic(fmt.Errorf("invalid register argument type; expected x86asm.Reg, got %T", arg))
+	}
+	return &Reg{
+		Reg:    reg,
+		parent: parent,
+	}
+}
+
+// --- [ memory reference ] ----------------------------------------------------
+
+// A Mem is a memory reference.
+type Mem struct {
+	// x86 memory reference.
+	x86asm.Mem
+	// Parent instruction; used to retrieve symbolic execution information.
+	parent *Inst
+}
+
+// NewMem returns a new memory reference argument with the given parent
+// instruction.
+func NewMem(arg x86asm.Arg, parent *Inst) *Mem {
+	mem, ok := arg.(x86asm.Mem)
+	if !ok {
+		panic(fmt.Errorf("invalid memory reference argument type; expected x86asm.Mem, got %T", arg))
+	}
+	return &Mem{
+		Mem:    mem,
+		parent: parent,
+	}
+}
+
+// Segment returns the segment register of the memory reference.
+func (mem *Mem) Segment() *Reg {
+	return NewReg(mem.Mem.Segment, mem.parent)
+}
+
+// Base returns the base register of the memory reference.
+func (mem *Mem) Base() *Reg {
+	return NewReg(mem.Mem.Base, mem.parent)
+}
+
+// Index returns the index register of the memory reference.
+func (mem *Mem) Index() *Reg {
+	return NewReg(mem.Mem.Index, mem.parent)
+}
+
+// ### [ Helper functions ] ####################################################
+
 // Addrs returns the addresses specified by the given argument. Addr specifies
 // the address of the terminator, and next the address of the next instruction.
 func (dis *Disasm) Addrs(arg x86asm.Arg, addr, next bin.Address) []bin.Address {

244269536a61ac7724136efa3336c1e7d1f4095a
mewmew <rnd0x00@gmail.com>
2017-08-25 09:56:58 +0200

    cmd/dump_imports: parse import table, IAT and INT

--- cmd/dump_imports/main.go
+++ cmd/dump_imports/main.go
@@ -1,4 +1,4 @@
-// The imports tool dumps the imports of a PE binary in NASM syntax.
+// The dump_imports tool dumps the imports of a PE binary in NASM syntax.
 package main
 
 import (
@@ -11,8 +11,10 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"sort"
 	"strings"
 
+	"github.com/kr/pretty"
 	"github.com/mewkiz/pkg/pathutil"
 	"github.com/mewrev/pe"
 	"github.com/pkg/errors"
@@ -136,6 +138,23 @@ import_table:
 
 `
 	bufINT.WriteString(intHeader[1:])
+
+	const iatHeader = `
+; === [ Import Address Tables (IATs) ] =========================================
+;
+;     Each Import Address Table (IAT) consists of an array of IMAGE_THUNK_DATA
+;     structures, which are terminated by an empty struct.
+;
+;     The linker will overwrite these DWORDs with the actuall address of the
+;     imported functions
+;
+; ------------------------------------------------------------------------------
+
+iat:
+
+`
+	bufIAT.WriteString(iatHeader[1:])
+	var dlls []*DLL
 	for {
 		if err := binary.Read(r, binary.LittleEndian, &impDesc); err != nil {
 			if errors.Cause(err) == io.EOF {
@@ -162,11 +181,24 @@ import_table:
 		fmt.Fprintf(buf, importDescFormat[1:], name, impDesc.Date, impDesc.ForwardChain, strings.Title(name), name)
 
 		fmt.Println("  => DLL name:", dllName)
-		dllIAT := data(impDesc.ImportAddressTableRVA)
-		r := bytes.NewReader(dllIAT)
+		dllINT := data(impDesc.ImportNameTableRVA)
+		r := bytes.NewReader(dllINT)
 		if err := dumpINT(name, r, bufINT, data); err != nil {
 			return errors.WithStack(err)
 		}
+
+		dllIAT := data(impDesc.ImportAddressTableRVA)
+		r = bytes.NewReader(dllIAT)
+		if err := dumpIAT(name, r, bufIAT, data); err != nil {
+			return errors.WithStack(err)
+		}
+		r = bytes.NewReader(dllIAT)
+		dll, err := parseDLL(dllName, r, data)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		dll.Addr = impDesc.DLLNameRVA
+		dlls = append(dlls, dll)
 	}
 	const importTableFooter = `
    import_table_size    equ     $ - import_table
@@ -176,9 +208,34 @@ import_table:
 `
 	buf.WriteString(importTableFooter[1:])
 
+	const intHeaderFooter = `
+; === [/ Import Name Tables (INTs) ] ===========================================
+
+`
+	bufINT.WriteString(intHeaderFooter[1:])
+
+	const iatHeaderFooter = `
+   iat_size             equ     $ - iat
+
+; === [/ Import Address Tables (IATs) ] ========================================
+
+`
+	bufIAT.WriteString(iatHeaderFooter[1:])
+
 	if _, err := buf.Write(bufINT.Bytes()); err != nil {
 		return errors.WithStack(err)
 	}
+
+	if _, err := buf.Write(bufIAT.Bytes()); err != nil {
+		return errors.WithStack(err)
+	}
+
+	less := func(i, j int) bool {
+		return dlls[i].Addr < dlls[j].Addr
+	}
+	sort.Slice(dlls, less)
+	pretty.Println(dlls)
+	dumpDLLs(dlls, buf)
 
 	if err := os.MkdirAll("_dump_", 0755); err != nil {
 		return errors.WithStack(err)
@@ -187,6 +244,48 @@ import_table:
 		return errors.WithStack(err)
 	}
 	return nil
+}
+
+func dumpDLLs(dlls []*DLL, w io.Writer) {
+
+	const dllsHeader = `
+; === [ dll and function names ] ===============================================
+;
+;    each dll stores an array of IMAGE_IMPORT_BY_NAME structures and a string
+;    corresponding to it's dll name.
+;
+; ------------------------------------------------------------------------------
+
+`
+	fmt.Fprint(w, dllsHeader[1:])
+	for _, dll := range dlls {
+		const dllHeaderFormat = `
+; --- [ %s ] ---------------------------------------------------------
+
+`
+		fmt.Fprintf(w, dllHeaderFormat[1:], dll.Name)
+		for _, f := range dll.Funcs {
+			const funcFormat = `
+imp_%s:
+                        dw      0x%04X
+                        db      '%s', 0x00 ; 0x%08X
+                        align 2, db 0x00
+
+`
+			fmt.Fprintf(w, funcFormat[1:], f.Name, f.Ordinal, f.Name, f.Addr)
+		}
+		const dllFooterFormat = `
+sz%s:
+                        db      '%s', 0x00 ; 0x%08X
+                        align 2, db 0x00
+
+`
+		fmt.Fprintf(w, dllFooterFormat[1:], strings.Title(strings.ToLower(strings.Replace(dll.Name, ".", "_", -1))), dll.Name, dll.Addr)
+	}
+	const dllsFooter = `
+; === [/ dll and function names ] ==============================================
+`
+	fmt.Fprint(w, dllsFooter[1:])
 }
 
 func dumpINT(dllName string, r io.Reader, w io.Writer, data func(addr uint32) []byte) error {
@@ -231,6 +330,102 @@ int_%s:
                         dd      imp_%s - IMAGE_BASE
 `
 			fmt.Fprintf(w, intEntryFormat[1:], funcName)
+		}
+	}
+	return nil
+}
+
+type DLL struct {
+	Name  string
+	Addr  uint32
+	Funcs []*Func
+}
+
+type Func struct {
+	Addr    uint32
+	Ordinal uint16
+	Name    string
+}
+
+func parseDLL(dllName string, r io.Reader, data func(addr uint32) []byte) (*DLL, error) {
+	dll := &DLL{
+		Name: dllName,
+	}
+	for {
+		var addr uint32
+		if err := binary.Read(r, binary.LittleEndian, &addr); err != nil {
+			if errors.Cause(err) == io.EOF {
+				break
+			}
+			return nil, errors.WithStack(err)
+		}
+		if addr == 0 {
+			break
+		}
+		if addr&0x80000000 != 0 {
+			// ordinal, nothing to do.
+			continue
+		}
+		f := &Func{
+			Addr: addr,
+		}
+		dll.Funcs = append(dll.Funcs, f)
+		if err := binary.Read(bytes.NewReader(data(addr)), binary.LittleEndian, &f.Ordinal); err != nil {
+			return nil, errors.WithStack(err)
+		}
+		f.Name = parseString(data(addr + 2))
+	}
+	less := func(i, j int) bool {
+		return dll.Funcs[i].Addr < dll.Funcs[j].Addr
+	}
+	sort.Slice(dll.Funcs, less)
+	return dll, nil
+}
+
+func dumpIAT(dllName string, r io.Reader, w io.Writer, data func(addr uint32) []byte) error {
+	const iatHeaderFormat = `
+; --- [ %s.dll ] ---------------------------------------------------------
+
+iat_%s:
+`
+	fmt.Fprintf(w, iatHeaderFormat[1:], dllName, dllName)
+	for {
+		var addr uint32
+		if err := binary.Read(r, binary.LittleEndian, &addr); err != nil {
+			if errors.Cause(err) == io.EOF {
+				break
+			}
+			return errors.WithStack(err)
+		}
+		if addr == 0 {
+			const iatNullEntry = `
+                        dd      0x00000000
+`
+			fmt.Fprintln(w, iatNullEntry[1:])
+			break
+		}
+		fmt.Printf("addr: 0x%08X\n", addr)
+		if addr&0x80000000 != 0 {
+			// addr is an encoded ordinal, not an address.
+			ordinal := addr &^ 0x80000000
+			fmt.Println("ordinal:", ordinal)
+			const iatOrdinalFormat = `
+  ia_%s_%d:
+                        dd      0x80000000 | %d
+`
+			fmt.Fprintf(w, iatOrdinalFormat[1:], dllName, ordinal, ordinal)
+		} else {
+			var ordinal uint16
+			if err := binary.Read(bytes.NewReader(data(addr)), binary.LittleEndian, &ordinal); err != nil {
+				return errors.WithStack(err)
+			}
+			funcName := parseString(data(addr + 2))
+			fmt.Printf("function: %s (%d)\n", funcName, ordinal)
+			const iatEntryFormat = `
+  ia_%s:
+                        dd      imp_%s - IMAGE_BASE
+`
+			fmt.Fprintf(w, iatEntryFormat[1:], funcName, funcName)
 		}
 	}
 	return nil

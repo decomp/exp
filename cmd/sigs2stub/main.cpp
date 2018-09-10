7ab531cacb94f3ba0701b58121a3e5eb9f80898b
Robin Eklind <rnd0x00@gmail.com>
2018-09-10 02:59:36 +0200

    cmd/sigs2stub: dump function stubs in assembly

--- cmd/sigs2stub/main.cpp
+++ cmd/sigs2stub/main.cpp
@@ -21,9 +21,9 @@ std::tuple<std::string, bool> read_file(char *path) {
 
 void dump_func(const clang::FunctionDecl *f) {
 	const clang::FunctionType *f_type = f->getFunctionType();
-	f_type->dump();
+	//f_type->dump();
 	clang::CallingConv cc = f_type->getCallConv();
-	std::cout << "calling convention: " << cc << std::endl;
+	//std::cout << "calling convention: " << cc << std::endl;
 	clang::SectionAttr *attr = f->getAttr<clang::SectionAttr>();
 	if (!attr) {
 		// unable to locate function address.
@@ -43,20 +43,54 @@ void dump_func(const clang::FunctionDecl *f) {
 		std::cerr << "unable to parse hexadecimal value '" << addr_str << "'" << std::endl;
 		return;
 	}
-	std::cout << "address: " << addr << std::endl;
-	for (clang::ParmVarDecl *param : f->parameters()) {
-		param->dump();
-		std::string param_name = param->getName().str();
-		clang::QualType param_type = param->getType();
-		std::cout << "param: " << param_name << std::endl;
+
+	//printf("times (0x%06X - _text_vstart) - ($ - $$) db 0xCC\n", addr);
+	printf("; address: 0x%06X\n", addr);
+	printf("%s:\n", f->getName().str().c_str());
+	printf("  push    ebp\n");
+	printf("  mov     ebp, esp\n");
+	llvm::ArrayRef<clang::ParmVarDecl *> params = f->parameters();
+	int nparams = f->getNumParams();
+	int param_stack_bytes = 0;
+	switch (cc) {
+	case clang::CC_X86FastCall:
+		// The first two arguments are passed in ecx and edx.
+		for (int arg_num = nparams; arg_num > 0; arg_num--) {
+			// TODO: handle param type; param->getType();
+			switch (arg_num) {
+			// The first two arguments are passed in ecx and edx.
+			case 1:
+				printf("  push    ecx                 ; arg_%d (%s)\n", arg_num, params[arg_num-1]->getName().str().c_str());
+				break;
+			case 2:
+				printf("  push    edx                 ; arg_%d (%s)\n", arg_num, params[arg_num-1]->getName().str().c_str());
+				break;
+			default:
+				printf("  push    DWORD [ebp + %d]    ; arg_%d (%s)\n", (arg_num-1)*4, arg_num, params[arg_num-1]->getName().str().c_str());
+				param_stack_bytes += 4;
+				break;
+			}
+		}
+		break;
+	default:
+		for (int arg_num = nparams; arg_num > 0; arg_num--) {
+			printf("  push    DWORD [ebp + %d]    ; arg_%d (%s)\n", (arg_num-1)*4, arg_num, params[arg_num-1]->getName().str().c_str());
+			param_stack_bytes += 4;
+		}
+		break;
 	}
+	printf("  call    [ia_%s]\n", f->getName().str().c_str());
+	printf("  mov     esp, ebp\n");
+	printf("  push    ebp\n");
+	printf("  ret     %d\n", param_stack_bytes);
+	printf("\n");
 }
 
 // visit_decl vists the declaration of the AST.
 bool visit_decl(void *ctx, const clang::Decl *decl) {
-	decl->dump();
+	//decl->dump();
 	const char *kind = decl->getDeclKindName();
-	std::cout << "kind: " << kind << std::endl;
+	//std::cout << "kind: " << kind << std::endl;
 	if (strcmp(kind, "Function") == 0) {
 		dump_func(decl->getAsFunction());
 	}
